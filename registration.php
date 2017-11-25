316d4232f8d5773a071254474d0a568557128bae
Bezigeboefjes <git@bezigeboefjes.nl>
2017-11-25 15:16:05 +0100

    Renamed to Bezigeboefjes fork

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Bold_OrderComment',
+        'Bezigeboefjes_OrderComment',
         __DIR__
     );
